ee36b8dba93276c8f0bfdfb6f354c84a0a7cea81
jeremysklarsky <jeremy.sklarsky@gmail.com>
2015-02-21 19:22:25 -0500

    remove comments

--- lib/lookup.rb
+++ lib/lookup.rb
@@ -1,16 +1,6 @@
 class Lookup
 
-  # def self.look_up(search)
-  #   data = JSON.parse(RestClient.get("http://d.yimg.com/autoc.finance.yahoo.com/autoc?query=#{search}&callback=YAHOO.Finance.SymbolSuggest.ssCallback"))
-  # end
-
   def self.look_up(search)
-    # data = Nokogiri::HTML(open("http://www.marketwatch.com/tools/quotes/lookup.asp?siteID=mktw&Lookup=#{search}&Country=us&Type=All"))
-    # results =[]
-    # data.search("div.results tr").each do |result|
-    #   results << [result.search("td.bottomborder").first.text, 
-    #               result.search("td.bottomborder")[1].text,
-    #               result.search("td.bottomborder")[2].text]
     term = search.gsub(" ", "")
     data = Nokogiri::HTML(open("http://www.dailyfinance.com/lookup/#{term}/usa"))
     results =[]
